5a12f376bf02b8d32e9330dbc6d39620ef95f7fd
Remyuu <zaomiaoe@outlook.com>
2023-11-22 15:17:52 +0800

    大抵是完成了ssr吧..

--- HW3/src/shaders/ssrShader/ssrFragment.glsl
+++ HW3/src/shaders/ssrShader/ssrFragment.glsl
@@ -19,6 +19,8 @@ varying highp vec4 vPosWorld;
 #define INV_PI 0.31830988618
 #define INV_TWO_PI 0.15915494309
 
+#define SAMPLE_NUM 5
+
 float Rand1(inout float p) {
   p = fract(p * .1031);
   p *= p + 33.33;
@@ -58,14 +60,27 @@ vec3 SampleHemisphereCos(inout float s, out float pdf) {
 
 void LocalBasis(vec3 n, out vec3 b1, out vec3 b2) {
   float sign_ = sign(n.z);
-  if (n.z == 0.0) {
-    sign_ = 1.0;
-  }
+  // if (n.z == 0.0) {
+  //   sign_ = 1.0;
+  // }
   float a = -1.0 / (sign_ + n.z);
   float b = n.x * n.y * a;
-  b1 = vec3(1.0 + sign_ * n.x * n.x * a, sign_ * b, -sign_ * n.x);
+  b1 = vec3(1.0 + sign_ * n.x * n.x * a,
+            sign_ * b,
+            -sign_ * n.x);
   b2 = vec3(b, sign_ + n.y * n.y * a, -n.y);
 }
+
+// void LocalBasis(vec3 n, out vec3 tangent, out vec3 bitangent) {
+//     vec3 helperVec;
+//     if (abs(n.x) < abs(n.y))
+//         helperVec = vec3(1.0, 0.0, 0.0);
+//     else
+//         helperVec = vec3(0.0, 1.0, 0.0);
+//     tangent = normalize(cross(helperVec, n));
+//     bitangent = normalize(cross(n, tangent));
+// }
+
 
 vec4 Project(vec4 a) {
   return a / a.w;
@@ -139,27 +154,96 @@ vec3 EvalDirectionalLight(vec2 screenUV) {
   return Li;
 }
 
-bool RayMarch(vec3 ori, vec3 dir, out vec3 hitPos) {
-  const int totalStepTimes = 150;
-  const float threshold = 0.0001;
-  float step = 0.05;
-  vec3 stepDir = normalize(dir) * step;
-  vec3 curPos = ori;
+/*
+ *  Test The Intersection from pos(world) to pos+dir*step*n(world)
+ */
 
-  for(int i = 0; i < totalStepTimes; i++) {
-    vec2 screenUV = GetScreenCoordinate(curPos);
-    float rayDepth = GetDepth(curPos);
-    float gBufferDepth = GetGBufferDepth(screenUV);
+// #define EPS 5e-2
+// #define TOTAL_STEP_TIMES 20
+// #define THRESHOLD 0.1
+// #define INIT_STEP 0.8
+// bool outScreen(vec3 curPos){
+//   vec2 uvScreen = GetScreenCoordinate(curPos);
+//   return any(bvec4(lessThan(uvScreen, vec2(0.0)), greaterThan(uvScreen, vec2(1.0))));
+// }
+// bool testDepth(vec3 nextPos){
+//   return GetDepth(nextPos) < GetGBufferDepth(GetScreenCoordinate(nextPos));
+// }
+// bool RayMarch(vec3 ori, vec3 dir, out vec3 hitPos) {
+//   float step = INIT_STEP;
+//   bool result = false, firstIn = false;
+//   vec3 nextPos, curPos = ori;
+//   for(int i = 0; i < TOTAL_STEP_TIMES; i++) {
+//     nextPos = curPos + dir * step;
+//     if(outScreen(curPos)) break;
+//     if(testDepth(nextPos)){ // 可以进步
+//       curPos += dir * step;
+//       if(firstIn) step *= 0.5;
+//       continue;
+//     }else{ // 过于进步了
+//       firstIn = true;
+//       if(step < EPS){
+//         float s1 = GetGBufferDepth(GetScreenCoordinate(curPos)) - GetDepth(curPos) + EPS;
+//         float s2 = GetDepth(nextPos) - GetGBufferDepth(GetScreenCoordinate(nextPos)) + EPS;
+//         if(s1 < THRESHOLD && s2 < THRESHOLD){
+//           hitPos = curPos + 2.0 * dir * step * s1 / (s1 + s2);
+//           result = true;
+//         }
+//         break;
+//       }
+//       if(firstIn) step *= 0.5;
+//     }
+//   }
+//   return result;
+// }
 
-    // Check if the ray has hit an object
-    if(rayDepth > gBufferDepth + threshold){
-      hitPos = curPos;
-      return true;
-    }
-    curPos += stepDir;
-  }
+
+#define INIT_STEP 0.8
+#define MAX_STEPS 20
+#define EPS 1e-2
+#define THRES 0.1
+bool outScreen(vec3 pos){
+  vec2 uv = GetScreenCoordinate(pos);
+  return any(bvec4(lessThan(uv, vec2(0.0)), greaterThan(uv, vec2(1.0))));
+}
+bool atFront(vec3 pos){
+  return GetDepth(pos) < GetGBufferDepth(GetScreenCoordinate(pos));
+}
+bool hasInter(vec3 pos, vec3 dir, out vec3 hitPos){
+  float d1 = GetGBufferDepth(GetScreenCoordinate(pos)) - GetDepth(pos) + EPS;
+  float d2 = GetDepth(pos + dir) - GetGBufferDepth(GetScreenCoordinate(pos + dir)) + EPS;
+  if(d1 < THRES && d2 < THRES){
+    hitPos = pos + dir * d1 / (d1 + d2);
+    return true;
+  }  
   return false;
 }
+
+bool RayMarch(vec3 ori, vec3 dir, out vec3 hitPos) {
+  bool intersect = false, firstinter = false;
+  float st = INIT_STEP;
+  vec3 current = ori;
+  for (int i = 0;i < MAX_STEPS;i++){
+    if(outScreen(current)){
+      break;
+    }
+    else if(atFront(current + dir * st)){
+      current += dir * st;
+    }else{
+      firstinter = true;
+      if(st < EPS){
+        if(hasInter(current, dir * st * 2.0, hitPos)){
+          intersect = true;
+        }
+        break;
+      }
+    }
+    if(firstinter)
+      st *= 0.5;
+  }
+  return intersect;
+}
+
 
 vec3 EvalSSR(vec3 wi, vec3 wo, vec2 screenUV) {
   vec3 worldNormal = GetGBufferNormalWorld(screenUV);
@@ -173,8 +257,6 @@ vec3 EvalSSR(vec3 wi, vec3 wo, vec2 screenUV) {
     return vec3(0.); 
   }
 }
-
-#define SAMPLE_NUM 10
 
 vec3 EvalIndirectionLight(vec3 wi, vec3 wo, vec2 screenUV){
   vec3 L_ind = vec3(0.0);
@@ -200,45 +282,29 @@ vec3 EvalIndirectionLight(vec3 wi, vec3 wo, vec2 screenUV){
   return L_ind;
 }
 
-// vec3 EvalIndirectionLight(vec3 pos){
-//   float pdf, seed = dot(pos, vec3(100.0));
-//   vec3 Li = vec3(0.0), dir, hitPos;
-//   vec3 normal = GetGBufferNormalWorld(GetScreenCoordinate(pos)), b1, b2;
-//   LocalBasis(normal, b1, b2);
-//   mat3 TBN = mat3(b1, b2, normal);
-//   for(int i = 0; i < SAMPLE_NUM;i++){
-//     dir = normalize(TBN * SampleHemisphereCos(seed, pdf));
-//     if(RayMarch(pos, dir, hitPos)){
-//       vec3 wo = normalize(uCameraPos - pos);
-//       vec3 L = EvalDiffuse(dir, wo, GetScreenCoordinate(pos)) / pdf;
-//       wo = normalize(uCameraPos - hitPos);
-//       vec3 wi = normalize(uLightDir);
-//       L *= EvalDiffuse(wi, wo, GetScreenCoordinate(hitPos)) * EvalDirectionalLight(GetScreenCoordinate(hitPos));
-//       Li += L;
-//     }
-//   }
-//   return Li / float(SAMPLE_NUM);
-// }
-
 // Main entry point for the shader
 void main() {
   vec3 wi = normalize(uLightDir);
   vec3 wo = normalize(uCameraPos - vPosWorld.xyz);
   vec2 screenUV = GetScreenCoordinate(vPosWorld.xyz);
 
+  // Main Light coefficient
+  float mainCoeff= 1.0;
   // Basic mirror-only SSR coefficient
-  float ssrCoeff = 0.0;
+  float ssrCoeff = 1.0;
   // Indirection Light coefficient
-  float indCoeff = 0.3;
+  float indCoeff = 1.0;
 
   // Direction Light
-  vec3 L_d = EvalDiffuse(wi, wo, screenUV) * EvalDirectionalLight(screenUV);
+  vec3 L_d = EvalDiffuse(wi, wo, screenUV) * EvalDirectionalLight(screenUV) * mainCoeff;
   // SSR Light
   vec3 L_ssr = EvalSSR(wi, wo, screenUV) * ssrCoeff;
   // Indirection Light
   vec3 L_i = EvalIndirectionLight(wi, wo, screenUV) * indCoeff;
 
-  vec3 result = L_d + L_ssr + L_i;
+  vec3 result = L_d + L_i;
+  // vec3 result = L_d + L_ssr;
+  // vec3 result = L_d + L_ssr + L_i;
   vec3 color = pow(clamp(result, vec3(0.0), vec3(1.0)), vec3(1.0 / 2.2));
   gl_FragColor = vec4(vec3(color.rgb), 1.0);
 }
